b6174b439c60e82e9a61aa3a1f7d2eab3ae8fe0e
prashantyella <yellp664@newschool.edu>
2026-05-09 13:18:36 -0400

    Fix web startup routing

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Colors } from '../src/constants/theme';
+
+export default function IndexScreen() {
+  return <View style={{ flex: 1, backgroundColor: Colors.bg.primary }} />;
+}
